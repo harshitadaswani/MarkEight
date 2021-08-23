43bcb677ba1bf250ca7828d2e810e638b0507c76
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-23 10:34:46 +0530

    ex08

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  var user = "Harshita";
+  var color = "blue";
+  //var username = prompt("Enter your name");
+  //var counter =0;
+  var [likeCounter, setCounter] = useState(0);
+
+  function likeHandler() {
+    console.log("liked!", counter);
+    //counter++;
+    setCounter(likeCounter);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1 style={{ backgroundColor: color }}>Hello {user}!!!</h1>
+      {/* <h1 style ={{backgroundColor: "red"}}>Hello {username}!!!</h1> */}
+      <button onClick={likeHandler}> Like me! </button>
     </div>
   );
 }
